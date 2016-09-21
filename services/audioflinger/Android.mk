2e134ee9c495b785e212b81e7be5ebf61cbeacec
SorrowfulMercies <SorrowfulMercies@gmail.com>
2016-09-21 03:37:15 -0500

    Update Android.mk

--- services/audioflinger/Android.mk
+++ services/audioflinger/Android.mk
@@ -93,8 +93,6 @@ endif
 endif
 #QTI Resampler
 
-LOCAL_CFLAGS += -Os -std=gnu++11
-
 LOCAL_MODULE:= libaudioflinger
 LOCAL_32_BIT_ONLY := true
 
@@ -197,7 +195,6 @@ endif
 #QTI Resampler
 
 LOCAL_MODULE := libaudioresampler
-LOCAL_CFLAGS += -std=gnu++11
 
 include $(BUILD_SHARED_LIBRARY)
 
